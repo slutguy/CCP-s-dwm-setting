129a5deea2f72d1319118f5aa23c8be99077abf4
slutguy <78467416+slutguy@users.noreply.github.com>
2022-09-10 20:48:40 +0800

    Add files via upload

---
+++ autostart.sh
@@ -0,0 +1,10 @@
+feh --bg-fill /home/reap/Desktop/Picther/pokemon/pokemon-1649594974671-1145.jpg
+picom -b
+tlp -start
+amixer sset Master 3%+ unmute &
+xsetroot -name  Todey\ Is\ A\ Better\ Day 
+xmodmap ~/.xmodmap &
+
+
+
+
